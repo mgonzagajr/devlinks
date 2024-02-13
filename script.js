726d2eb162898e56dc9b3d66183ae2d0817f048c
Márcio J R Gonzaga Jr <marciogonzagajr@gmail.com>
2024-02-13 12:09:03 -0300

    Preview image and update on profile image

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function toggleMode() {
   const img = document.querySelector('#profile img')
 
   if (html.classList.contains('light-theme')) {
-    img.setAttribute('src', './assets/avatar.png')
-  } else {
     img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
   }
 }
