cd122080b6b64afcae26150a9802ef383057ede5
Márcio J R Gonzaga Jr <marciogonzagajr@gmail.com>
2024-02-13 11:37:39 -0300

    First commit :tada:

---
+++ script.js
@@ -0,0 +1,15 @@
+const switchButton = document.querySelector('#switch')
+switchButton.addEventListener('click', toggleMode)
+
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light-theme')
+
+  const img = document.querySelector('#profile img')
+
+  if (html.classList.contains('light-theme')) {
+    img.setAttribute('src', './assets/avatar.png')
+  } else {
+    img.setAttribute('src', './assets/avatar-light.png')
+  }
+}
